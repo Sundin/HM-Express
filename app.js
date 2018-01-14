6a3c85777b32deba0a46d9a7ceb5979db4ca9e3d
Gustav Sundin <sundin666@gmail.com>
2018-01-14 11:44:22 +0100

    Catch unhandled rejections

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// Catch unhandled rejections on the global level:
+// Make sure to use Node.js built-in Error type: Promise.reject(new Error("Something bad happened"));
+process.on('unhandledRejection', error => {
+    console.log('Unhandled Rejection', error.message);
+});
