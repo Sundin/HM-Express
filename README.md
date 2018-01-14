858c5110a5bdbccdfe5d481111fc6c971e047e8e
Gustav Sundin <sundin666@gmail.com>
2018-01-14 12:04:16 +0100

    Added dotenv for handling environment variables

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ A boilerplate project for an Express backend.
     
 ## Dependencies
 * [Express](https://expressjs.com/)
-* [ESLint](https://eslint.org/)
+* [dotenv](https://github.com/motdotla/dotenv): For handling environment variables.
+* [ESLint](https://eslint.org/): A good linter.
--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+'use strict';
+
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
 
 // Catch unhandled rejections on the global level:
 // Make sure to use Node.js built-in Error type: Promise.reject(new Error("Something bad happened"));
